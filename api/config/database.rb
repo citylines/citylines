2e0db9791a807e630d794c75a4d126e31b39601a
Bruno Salerno <br.salerno@gmail.com>
2017-02-19 17:29:55 +0100

    Improves db config file

--- api/config/database.rb
+++ api/config/database.rb
@@ -1,18 +1,11 @@
 require 'yaml'
 
-config = {}
+connection = ENV['DATABASE_URL']
 
-begin
-    config = YAML::load_file('api/config/database.yml')
-rescue
-    config['username'] = ENV['DB_USERNAME'];
-    config['password'] = ENV['DB_PASSWORD'];
-    config['host'] = ENV['DB_HOST'];
-    config['port'] = ENV['DB_PORT'];
-    config['database'] = ENV['DB_NAME'];
+unless connection
+  config = YAML::load_file('api/config/database.yml')
+  connection = "postgres://#{config['username']}:#{config['password']}@#{config['host']}:#{config['port']}/#{config['database']}"
 end
-
-connection = "postgres://#{config['username']}:#{config['password']}@#{config['host']}:#{config['port']}/#{config['database']}"
 
 DB = Sequel.connect(connection)
 
