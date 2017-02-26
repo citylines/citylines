a5d6739ec133168d744f365b49bd85e76debda72
Bruno Salerno <br.salerno@gmail.com>
2017-02-26 23:33:50 +0100

    Fixes start_year min year. Fixes #32

--- api/lib/start_year.rb
+++ api/lib/start_year.rb
@@ -1,7 +1,8 @@
 module StartYear
   def after_save
-    year = self.buildstart || self.opening
-    if year < self.city.start_year && year > 1800
+    year = [self.buildstart, self.opening].select{|y| y.is_a?(Numeric) && y > 0}.min
+
+    if year && year < self.city.start_year && year > 1800
       self.city.start_year = year
       self.city.save
     end
