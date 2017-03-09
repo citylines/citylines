683235bb295a8e1a101973d021b508d05c70961d
Bruno Salerno <br.salerno@gmail.com>
2017-03-08 23:26:38 -0300

    Adds i18n for cookie components

--- client/locales/en.js
+++ client/locales/en.js
@@ -12,5 +12,17 @@ export default {
     auth: {
       log_in: 'Log in',
       log_in_with_google: 'Log in with Google'
+    },
+    cookie_notice: {
+      notice: 'This website uses cookies. If you continue to use this website you accept our cookies policy.',
+      accept: 'Accept',
+      info: 'Information about our cookies policy',
+      text: {
+        title: 'Our cookies policy',
+        p1: 'Cookies are small pieces of information that are stored in your web browser and that can be accessed by one or several websites.',
+        p2: 'Citylines.co uses own and third-party cookies with the purpose of improving the user experience. We use own cookies to remember which user has logged in and to know whether the user has pressed or not the Accept button in the cookies banner. Regarding the third-party cookies, Google uses cookies in this website to store information related to the Google Login and to Google Analytics. All these cookies are persistent and are used only with the described scope.',
+        p3: 'You can disable the cookies in this website, but this could affect certain features of Citilines.co.',
+        p4: 'You can find more information about cookies at <a target="_blank" className="c-link" href="http://www.aboutcookies.org">aboutcookies.org</a>, including guides on how to see which cookies are installed in your browser, or how to uninstall them.'
+      }
     }
   }
--- client/src/components/cookie-notice.js
+++ client/src/components/cookie-notice.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router';
+import Translate from 'react-translate-component';
 
 class CookieNotice extends Component {
   render() {
     return (
     <div className="cookie-notice-container c-list c-list--unstyled">
-      <p>Este sitio web usa cookies. Al navegarlo usted acepta el uso que hacemos de ellas.<button onClick={this.props.onAccept} className="c-button c-button--info">Aceptar</button><br /><Link className="c-link" to="/cookies">Información sobre las cookies que usamos</Link></p>
+      <p><Translate content="cookie_notice.notice" /><Translate component="button" onClick={this.props.onAccept} className="c-button c-button--info" content="cookie_notice.accept" /><br /><Translate component={Link} to="/cookies" content="cookie_notice.info" /></p>
     </div>
     )
   }
