6a96c2b23430a04ed68f716699acfa936eb680fc
Bruno Salerno <br.salerno@gmail.com>
2017-03-12 12:14:25 -0300

    Improves accuracy of length calculations for cities with no srid

--- api/lib/length.rb
+++ api/lib/length.rb
@@ -1,14 +1,13 @@
 module Length
-    def srid
-        puts "Length#self: #{self}"
-        city.srid || 3857
-    end
-
     def set_length
         self.length = calculate_length
     end
 
     def calculate_length
-        Sequel.lit("ST_Length(ST_Transform(geometry, #{srid}))::int")
+      if city.srid
+        Sequel.lit("ST_Length(ST_Transform(geometry, #{city.srid}))::int")
+      else
+        Sequel.lit(%q(ST_Length_Spheroid(geometry, 'SPHEROID["GRS_1980",6378137,298.257222101]')::int))
+      end
     end
 end
