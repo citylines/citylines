00fa83a2f64b31bd4357c4d46bc6e4ba01336cab
Bruno Salerno <br.salerno@gmail.com>
2017-03-12 12:22:40 -0300

    Fixes deprecated POSTGIS function name

--- api/lib/length.rb
+++ api/lib/length.rb
@@ -7,7 +7,7 @@ module Length
       if city.srid
         Sequel.lit("ST_Length(ST_Transform(geometry, #{city.srid}))::int")
       else
-        Sequel.lit(%q(ST_Length_Spheroid(geometry, 'SPHEROID["GRS_1980",6378137,298.257222101]')::int))
+        Sequel.lit(%q(ST_LengthSpheroid(geometry, 'SPHEROID["GRS_1980",6378137,298.257222101]')::int))
       end
     end
 end
