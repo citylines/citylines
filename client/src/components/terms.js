205dc61fb2d7a33869f0c1ccaf34c77ca26665da
Bruno Salerno <br.salerno@gmail.com>
2017-03-17 23:55:25 -0300

    Adds terms component

---
+++ client/src/components/terms.js
@@ -0,0 +1,45 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import {Link} from 'react-router';
+import Translate from 'react-translate-component';
+
+class Terms extends Component {
+  componentDidMount() {
+    this.checkCookiesHash();
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.location.hash != prevProps.location.hash) {
+      this.checkCookiesHash();
+    }
+  }
+
+  checkCookiesHash() {
+    if (this.props.location.hash.includes('cookies')) {
+      ReactDOM.findDOMNode(this.refs.cookies).scrollIntoView();
+    }
+  }
+
+  render() {
+    return (
+        <div className="o-container o-container--small">
+          <div className="u-letter-box--large">
+            <Translate component="h1" className="c-heading" content="cookie_notice.text.title" />
+            <Translate component="h3" className="c-heading" content="cookie_notice.text.title" />
+            <p><Translate content="cookie_notice.text.p1" /></p>
+            <p><Translate content="cookie_notice.text.p2" /></p>
+            <p><Translate content="cookie_notice.text.p3" /></p>
+            <p><Translate content="cookie_notice.text.p4" unsafe /></p>
+
+            <Translate component="h3" className="c-heading" content="cookie_notice.text.title" id="cookies" ref="cookies"/>
+            <p><Translate content="cookie_notice.text.p1" /></p>
+            <p><Translate content="cookie_notice.text.p2" /></p>
+            <p><Translate content="cookie_notice.text.p3" /></p>
+            <p><Translate content="cookie_notice.text.p4" unsafe /></p>
+          </div>
+        </div>
+        )
+  }
+}
+
+export default Terms
