e2d372d0f9a9c56fe9efdf5f4f70b2db8ef32b02
Bruno Salerno <br.salerno@gmail.com>
2017-04-14 19:01:43 -0300

    Improves user's created_features query and var naming

--- api/app/helpers/user_helpers.rb
+++ api/app/helpers/user_helpers.rb
@@ -2,32 +2,23 @@ module UserHelpers
   def user_cities(user_id)
     h = {}
 
-    CreatedFeature
-    .where(user_id: user_id)
-    .left_join(:sections, :created_features__feature_class => 'Section', :created_features__feature_id => :sections__id)
-    .each do |created_feature|
-      city_id = created_feature[:city_id]
+    created_features_query = %{
+      select city_id, feature_class, sum(sections.length), count(distinct feature_id) from created_features left join
+        sections on (feature_class = 'Section' and created_features.feature_id = sections.id)
+        where user_id = #{user_id} group by city_id, feature_class
+    }
+
+    DB.fetch(created_features_query).each do |group|
+      city_id = group[:city_id]
       h[city_id] ||= {}
       h[city_id][:created_features] ||= {section_count: 0, section_length: 0, station_count: 0}
 
-      if created_feature[:feature_class] == 'Section'
-        h[city_id][:created_features][:section_count] += 1
-
-        unless created_feature[:length]
-          # If there is no length, it probably means that the section has been deleted.
-          # Thats why we try to fetch the length from the SectionBackup
-
-          backup = SectionBackup.where(original_id: created_feature[:feature_id]).first
-
-          next unless backup
-
-          created_feature[:length] = backup.length
-        end
-
-        h[city_id][:created_features][:section_length] += created_feature[:length]
-      else
-        h[city_id][:created_features][:station_count] += 1
+      if group[:sum]
+        h[city_id][:created_features][:section_length] = group[:sum]
       end
+
+      key = "#{group[:feature_class].downcase}_count".to_sym
+      h[city_id][:created_features][key] = group[:count]
     end
 
     modified_features_query = %{
@@ -36,13 +27,13 @@ module UserHelpers
         group by city_id, feature_class
     }
 
-    DB.fetch(modified_features_query).each do |modified_feature|
-      city_id = modified_feature[:city_id]
+    DB.fetch(modified_features_query).each do |group|
+      city_id = group[:city_id]
       h[city_id] ||= {}
       h[city_id][:modified_features] ||= {section_count: 0, station_count: 0}
 
-      key = "#{modified_feature[:feature_class].downcase}_count".to_sym
-      h[city_id][:modified_features][key] = modified_feature[:count]
+      key = "#{group[:feature_class].downcase}_count".to_sym
+      h[city_id][:modified_features][key] = group[:count]
     end
 
     # TODO:
