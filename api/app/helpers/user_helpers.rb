f13f941879f7d65f53a9d1c3e477313d4d6de5dc
Bruno Salerno <br.salerno@gmail.com>
2017-04-14 19:16:37 -0300

    Adds deleted features to user activity stats

--- api/app/helpers/user_helpers.rb
+++ api/app/helpers/user_helpers.rb
@@ -14,7 +14,7 @@ module UserHelpers
       h[city_id][:created_features] ||= {section_count: 0, section_length: 0, station_count: 0}
 
       if group[:sum]
-        h[city_id][:created_features][:section_length] = group[:sum]
+        h[city_id][:created_features][:section_length] = (group[:sum] * 1.0 / 1000).round(2)
       end
 
       key = "#{group[:feature_class].downcase}_count".to_sym
@@ -36,8 +36,14 @@ module UserHelpers
       h[city_id][:modified_features][key] = group[:count]
     end
 
-    # TODO:
-    # - Removed Features
+    DeletedFeature.where(user_id: user_id).group_and_count(:city_id, :feature_class).each do |group|
+      city_id = group[:city_id]
+      h[city_id] ||= {}
+      h[city_id][:deleted_features] ||= {section_count: 0, station_count: 0}
+
+      key = "#{group[:feature_class].downcase}_count".to_sym
+      h[city_id][:deleted_features][key] = group[:count]
+    end
 
     h
   end
