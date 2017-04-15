e02c51fe5e8ff923f39ea70dcad41caad88e7d8e
Bruno Salerno <br.salerno@gmail.com>
2017-04-15 17:16:43 -0300

    Shows info in User component

--- client/src/components/user.js
+++ client/src/components/user.js
@@ -9,6 +9,8 @@ class User extends Component {
   constructor(props, context) {
     super(props, context);
 
+    this.state = {}
+
     this.userId = this.props.params.user_id;
 
     this.bindedOnChange = this.onChange.bind(this);
@@ -30,9 +32,52 @@ class User extends Component {
     UserStore.load(this.userId);
   }
 
+  km(city) {
+    return city.created_features && city.created_features.section_length > 0 ? city.created_features.section_length : null;
+  }
+
+  modifiedFeaturesCount(city) {
+    let total = 0;
+    ['modified_features', 'deleted_features'].map(attr => {
+      if (!city[attr]) return;
+      total += city[attr].section_count
+      total += city[attr].station_count
+    });
+    return total > 0 ? total : null
+  }
+
   render() {
     console.log(this.state);
-    return null;
+    return (
+      <div className="o-container o-container--small">
+        <div className="u-letter-box--large">
+          <h1 className="c-heading">{this.state.name}</h1>
+          <h2 className="c-heading">Ciudades editadas</h2>
+
+          <table className="c-table c-table--striped">
+            <thead className="c-table__head">
+              <tr className="c-table__row c-table__row--heading">
+                <th className="c-table__cell">Ciudad</th>
+                <th className="c-table__cell">Km creados</th>
+                <th className="c-table__cell">Elementos modificados o borrados</th>
+              </tr>
+            </thead>
+            <tbody className="c-table__body">
+              {this.state.cities && this.state.cities.map(city =>
+                <tr key={city.city.name} className="c-table__row">
+                  <td className="c-table__cell"><Link to={`/${city.city.url_name}`} className="c-link">{city.city.name}</Link></td>
+                  <td className="c-table__cell">{this.km(city)}</td>
+                  <td className="c-table__cell">{this.modifiedFeaturesCount(city)}</td>
+                </tr>
+              )}
+            </tbody>
+
+            <caption className="c-table__caption">Ciudades ordenadas por kms creados y por número de elementos modificados</caption>
+          </table>
+
+        </div>
+      </div>
+    )
   }
 }
 
