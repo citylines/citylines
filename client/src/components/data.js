c9215e8134cdaf1a8bf71abae84e6f60ca641083
Bruno Salerno <br.salerno@gmail.com>
2017-04-15 19:14:38 -0300

    Moves title inside data screen

--- client/src/components/data.js
+++ client/src/components/data.js
@@ -29,8 +29,9 @@ class Data extends Component {
           <div className="u-letter-box--large">
             <Translate component="h1" className="c-heading" content="data.title" />
 
-            <Translate component="h3" className="c-heading" content="data.download"/>
             <p><Translate content="data.license" unsafe />. <Translate content="data.see_terms_1" /> <Translate component={Link} className="c-link" to="/terms" content="data.see_terms_2" />.</p>
+
+            <Translate component="h3" className="c-heading" content="data.download"/>
             { Object.entries(this.datasets()).map(entry => {
               const label = entry[0];
               const url = entry[1];
