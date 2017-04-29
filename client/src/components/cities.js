f1db5b1d847fcd356ebb512618af10ec83ae7182
Bruno Salerno <br.salerno@gmail.com>
2017-04-29 20:17:12 -0300

    Revert "Hides contributors title until it's content is loaded"
    This reverts commit fad8499891f9135bb66ca8ea784423b245980fea.

--- client/src/components/cities.js
+++ client/src/components/cities.js
@@ -120,7 +120,6 @@ class Cities extends Component {
           </div>
         </div>
 
-        { this.state.topContributors.length ?
         <div className="o-container o-container--small">
           <div className="u-letter-box--large" style={{textAlign: 'center'}}>
             <Translate component="h2" className="c-heading" content="cities.contributors.list.title" />
@@ -143,7 +142,7 @@ class Cities extends Component {
               </ol>
             </div>
           </div>
-        </div> : '' }
+        </div>
 
         <div className="o-container o-container--medium" style={{textAlign: 'center', marginTop:30}}>
           <div className="u-letter-box--super">
