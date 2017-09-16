5fa8f73edd50a6e39c55aa21adef5adadd9dc9f3
Bruno Salerno <br.salerno@gmail.com>
2017-09-15 21:53:40 -0300

    Removes remaining user icon

--- client/src/components/main.js
+++ client/src/components/main.js
@@ -72,7 +72,7 @@ class Main extends Component {
                 <img src="/img/citylines-navbar.svg" className="navbar-logo" />
               </Link>
               { this.state.username ?
-              <Link to={`/user/${this.state.userid}`} className="c-nav__item c-nav__item--right"><i className="fa fa-user-circle-o"></i> {this.state.username}</Link>  :
+              <Link to={`/user/${this.state.userid}`} className="c-nav__item c-nav__item--right">{this.state.username}</Link>  :
               <Link to="/auth" className="c-nav__item c-nav__item--right"><Translate content="main.log_in" /></Link> }
               <Link to="/terms" className="c-nav__item c-nav__item--right">
                 <Translate content="terms.title" />
