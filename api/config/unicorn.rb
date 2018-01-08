f6c9132929ffb077631d8c0b0b51747f2c9081fb
Bruno Salerno <br.salerno@gmail.com>
2018-01-08 19:17:01 -0300

    Adds working_directory

--- api/config/unicorn.rb
+++ api/config/unicorn.rb
@@ -1,6 +1,9 @@
 worker_processes 4
 preload_app true
 
+app_path = File.expand_path(File.dirname(__FILE__) + "/../../")
+working_directory app_path
+
 before_fork do |server, worker|
   DB.disconnect
 end
