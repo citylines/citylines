194e8fd54d773121d2ed1d50f59259715a18e005
Bruno Salerno <br.salerno@gmail.com>
2018-01-08 19:20:41 -0300

    Handle signals

--- api/config/unicorn.rb
+++ api/config/unicorn.rb
@@ -5,5 +5,16 @@ app_path = File.expand_path(File.dirname(__FILE__) + "/../../")
 working_directory app_path
 
 before_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    Process.kill 'QUIT', Process.pid
+  end
+
   DB.disconnect
+end
+
+after_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+  end
 end
