9f4af713844b62ae3a8e907c4b1bba29330f62da
Bruno Salerno <br.salerno@gmail.com>
2018-02-21 19:48:11 -0300

    Delete systems params if lines param is present

--- client/src/components/city/city-view.js
+++ client/src/components/city/city-view.js
@@ -59,6 +59,11 @@ class CityView extends PureComponent {
     // We delete null params
     Object.keys(params).forEach((key) => (params[key] == null) && delete params[key]);
 
+    // We delete systems param if the new ones include the lines param
+    if (newParams.lines && this.params().systems) {
+      delete params.systems;
+    }
+
     // If new params are equal to the current ones, we don't push the state to the
     // browser history
     if (JSON.stringify(params) === JSON.stringify(this.params())) return;
