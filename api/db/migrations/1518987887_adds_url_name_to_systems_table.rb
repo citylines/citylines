827079e6e26902f5fe3289a14396bd46d5f3b691
Bruno Salerno <br.salerno@gmail.com>
2018-02-21 19:38:57 -0300

    Adds system_backups migrations

--- api/db/migrations/1518987887_adds_url_name_to_systems_table.rb
+++ api/db/migrations/1518987887_adds_url_name_to_systems_table.rb
@@ -8,6 +8,10 @@ Sequel.migration do
       add_column :url_name, String
     end
 
+    alter_table :system_backups do
+      add_column :url_name, String
+    end
+
     from(:systems).each do |system|
       system_name = system[:name] || 'unnamed'
       url_name = "#{system[:id]}-#{system_name.strip.accentless.gsub(/\s|\//,'-').downcase}"
@@ -17,6 +21,10 @@ Sequel.migration do
 
   down do
     alter_table :systems do
+      drop_column :url_name
+    end
+
+    alter_table :system_backups do
       drop_column :url_name
     end
   end
