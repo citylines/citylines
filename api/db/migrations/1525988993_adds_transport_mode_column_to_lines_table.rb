4ee4a8c28dc3c593863b6e3a1d14139c3abb84ef
Bruno Salerno <br.salerno@gmail.com>
2018-05-11 20:21:37 -0300

    Fixes migration

--- api/db/migrations/1525988993_adds_transport_mode_column_to_lines_table.rb
+++ api/db/migrations/1525988993_adds_transport_mode_column_to_lines_table.rb
@@ -1,7 +1,21 @@
 Sequel.migration do
-  change do
+  up do
     alter_table :lines do
       add_column :transport_mode_id, Integer
+    end
+
+    alter_table :line_backups do
+      add_column :transport_mode_id, Integer
+    end
+  end
+
+  down do
+    alter_table :lines do
+      drop_column :transport_mode_id
+    end
+
+    alter_table :line_backups do
+      drop_column :transport_mode_id
     end
   end
 end
