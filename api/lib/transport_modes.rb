c43abe069aab5de120c08122c0f14477e67143f9
Bruno Salerno <br.salerno@gmail.com>
2018-05-10 21:54:05 -0300

    Improve transport modes width

--- api/lib/transport_modes.rb
+++ api/lib/transport_modes.rb
@@ -1,13 +1,13 @@
 module TransportModes
   TRANSPORT_MODES = {
     0 => { name: 'Default', width: 6, min_width: 6},
-    1 => { name: 'High Speed', width: 10, min_width: 3},
-    2 => { name: 'Interurban train', width: 9, min_width: 3},
-    3 => { name: 'Commuter train', width: 8, min_width: 2},
+    1 => { name: 'High Speed', width: 9, min_width: 3},
+    2 => { name: 'Interurban train', width: 8, min_width: 3},
+    3 => { name: 'Commuter train', width: 7, min_width: 2},
     4 => { name: 'Heavy Metro', width: 6, min_width: 2},
     5 => { name: 'Light Rail', width: 4, min_width: 2},
-    6 => { name: 'BRT', width: 3, min_width: 1},
-    7 => { name: 'People mover', width: 2, min_width: 2},
+    6 => { name: 'BRT', width: 4, min_width: 1},
+    7 => { name: 'People mover', width: 3, min_width: 2},
     8 => { name: 'Bus', width: 1, min_width: 1},
   }
 end
