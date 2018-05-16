e673279851b7d65683650db4c0e9417b18595bd3
Bruno Salerno <br.salerno@gmail.com>
2018-05-16 19:46:56 -0300

    Improves popup code

--- client/src/components/city/feature-popup-content.js
+++ client/src/components/city/feature-popup-content.js
@@ -10,10 +10,6 @@ class FeaturePopupContent extends Component {
     return {color: p.lineLabelColor, backgroundColor: p.lineColor, marginLeft: 0, marginRight: 5, boxShadow: (p.lineLabelColor === '#000' ? '0 0 1px rgba(0,0,0,0.5)' : null)};
   }
 
-  validFeatureValue(value) {
-    return (value !== null && value !== 999999)
-  }
-
   groupedSystems(linesInfo) {
     let systems = {};
 
@@ -28,21 +24,12 @@ class FeaturePopupContent extends Component {
     return systems;
   }
 
-  stationTransportModes() {
-    return [ ...new Set(this.fProps().lines.map(l => l.transport_mode_name)) ].filter(e => e && e != 'default');
-  }
-
-  transportModeLabel(tm, key = null) {
-    return <Translate key={key} className="c-badge c-badge--ghost popup-transport-mode" content={`transport_modes.${tm}`} />;
-  }
-
   isStation() {
     return this.fProps().klass === 'Station';
   }
 
   render() {
     const fProps = this.fProps();
-    const checkBoxId = `${fProps.klass}-${fProps.id}`;
 
     return (
         <div className="c-text popup-feature-info">
@@ -69,26 +56,58 @@ class FeaturePopupContent extends Component {
               </li>
             </div>
           }
-          <input className="popup-data-checkbox" id={checkBoxId} type='checkbox'></input>
-          <div className="popup-data">
-            {
-              this.isStation() &&
-              <li className="c-list__item">{this.stationTransportModes().map(t => this.transportModeLabel(t, t))}</li>
-            }
-            {
-              !this.isStation() && fProps.transport_mode_name && fProps.transport_mode_name != 'default' &&
-                <li className="c-list__item">{this.transportModeLabel(fProps.transport_mode_name)}</li>
-            }
-            <li className="c-list__item popup-data-title">
-              { !this.isStation() && <Translate content="city.popup.track" /> }
-            </li>
-            { fProps.buildstart ? <li className="c-list__item"><Translate content="city.popup.buildstart" with={{year: fProps.buildstart}} /></li> : ''}
-            { this.validFeatureValue(fProps.opening) ? <li className="c-list__item"><Translate content="city.popup.opening" with={{year: fProps.opening}} /></li> : ''}
-            { this.validFeatureValue(fProps.closure) ? <li className="c-list__item"><Translate content="city.popup.closure" with={{year: fProps.closure}} /></li> : ''}
-            { fProps.length ? <li className="c-list__item"><Translate content="city.popup.length" with={{km: (parseFloat(fProps.length)/1000).toFixed(2)}} /></li> : ''}
-          </div>
-          <label htmlFor={checkBoxId} className="c-link popup-data-toggle fa fa-info-circle"></label>
+          <DetailedData
+            isStation={this.isStation()}
+            lines={fProps.lines}
+            transport_mode_name={fProps.transport_mode_name}
+            buildstart={fProps.buildstart}
+            opening={fProps.opening}
+            closure={fProps.closure}
+            length={fProps.length}
+            id={`${fProps.klass}-${fProps.id}`}
+          />
         </ul>
+      </div>
+    )
+  }
+}
+
+class DetailedData extends Component {
+  transportModes() {
+    let allModes = [];
+
+    if (this.props.isStation) {
+      allModes= this.props.lines.map(l => l.transport_mode_name);
+    } else {
+      allModes.push(this.props.transport_mode_name);
+    }
+
+    return [ ...new Set(allModes) ].filter(e => e && e != 'default');
+  }
+
+  validFeatureValue(value) {
+    return (value !== null && value !== 0 && value !== 999999);
+  }
+
+  render() {
+    return (
+      <div>
+        <input className="popup-data-checkbox" id={this.props.id} type='checkbox'></input>
+        <div className="popup-data">
+          <li className="c-list__item">
+            { this.transportModes().map(t =>
+               <Translate key={t} className="c-badge c-badge--ghost popup-transport-mode" content={`transport_modes.${t}`} />
+            ) }
+          </li>
+          <li className="c-list__item popup-data-title">
+            { !this.props.isStation && <Translate content="city.popup.track" /> }
+          </li>
+          { this.validFeatureValue(this.props.buildstart) ? <li className="c-list__item"><Translate content="city.popup.buildstart" with={{year: this.props.buildstart}} /></li> : ''}
+          { this.validFeatureValue(this.props.opening) ? <li className="c-list__item"><Translate content="city.popup.opening" with={{year: this.props.opening}} /></li> : ''}
+          { this.validFeatureValue(this.props.closure) ? <li className="c-list__item"><Translate content="city.popup.closure" with={{year: this.props.closure}} /></li> : ''}
+          { this.props.length ? <li className="c-list__item"><Translate content="city.popup.length" with={{km: (parseFloat(this.props.length)/1000).toFixed(2)}} /></li> : ''}
+        </div>
+        <label htmlFor={this.props.id} className="c-link popup-data-toggle fa fa-info-circle"></label>
       </div>
     )
   }
