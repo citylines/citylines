80ca61156586178b379fe35f5407a0abb2bedf02
Bruno Salerno <br.salerno@gmail.com>
2018-05-19 16:46:23 -0300

    Adds remaining frontend tests

--- client/__tests__/lib/style_test.js
+++ client/__tests__/lib/style_test.js
@@ -67,4 +67,69 @@ describe("calculate", () => {
       expect(style.calculate('stations', 'opening')).toEqual(expectedStyle);
     });
   });
+
+  describe("sections", () => {
+    it("should return the hover style", () => {
+      const expectedStyle = {
+        'line-color': '#000',
+        'line-opacity': 0.4,
+        'line-offset': {
+          type: 'identity',
+          property: 'offset'
+        },
+        'line-width': {
+          type: 'identity',
+          property: 'width'
+        }
+      }
+
+      const style = new Style([]);
+
+      expect(style.calculate('sections', 'hover')).toEqual(expectedStyle);
+    });
+
+    it("should return the buildstart style", () => {
+      const expectedStyle = {
+        'line-color': '#A4A4A4',
+        'line-offset': {
+          type: 'identity',
+          property: 'offset'
+        },
+        'line-width': {
+          type: 'identity',
+          property: 'width'
+        }
+      }
+
+      const style = new Style([]);
+
+      expect(style.calculate('sections', 'buildstart')).toEqual(expectedStyle);
+    });
+
+    it("should return the opening style", () => {
+      const expectedStyle = {
+        'line-color': {
+          type: 'categorical',
+          property: 'line_url_name',
+          stops: [
+            ['shared-station', '#000000'],
+            ['a', '#zzz'],
+            ['b', '#xxx']
+          ]
+        },
+        'line-offset': {
+          type: 'identity',
+          property: 'offset'
+        },
+        'line-width': {
+          type: 'identity',
+          property: 'width'
+        }
+      }
+
+      const style = new Style([{url_name: 'a', color: '#zzz'}, {url_name: 'b', color: '#xxx'}]);
+
+      expect(style.calculate('sections', 'opening')).toEqual(expectedStyle);
+    });
+  });
 });
