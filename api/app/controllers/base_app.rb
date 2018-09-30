3f7d3ddf02085feefe969e96583036470fa6c9c6
Bruno Salerno <br.salerno@gmail.com>
2018-09-30 20:18:59 +0200

    Use https for assets

--- api/app/controllers/base_app.rb
+++ api/app/controllers/base_app.rb
@@ -5,6 +5,7 @@ class BaseApp < App
   set :assets_paths, %w(../../client/assets)
   set :assets_precompile, %w(bundle.js *.css *.png *.svg)
   set :assets_host, "cdn.citylines.co"
+  set :assets_protocol, :https
 
   register Sinatra::AssetPipeline
 
