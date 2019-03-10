b0f0d474cafccfe22c50f6a92c0b9aacb4512c58
Bruno Salerno <br.salerno@gmail.com>
2019-03-10 11:26:05 +0100

    Query features without geometry

--- api/app/helpers/city_helpers.rb
+++ api/app/helpers/city_helpers.rb
@@ -64,7 +64,7 @@ module CityHelpers
   def formatted_lines_features_collection(city, type)
     geoms = features_geometry(city, type)
 
-    features = features_query(city, type).all.map do |el|
+    features = features_query(city, type).without_geometry.all.map do |el|
       el.formatted_feature(geometry: geoms[el.id])
     end.flatten
 
@@ -75,7 +75,7 @@ module CityHelpers
   def lines_features_collection(city, type)
     geoms = features_geometry(city, type)
 
-    features = features_query(city, type).all.map do |el|
+    features = features_query(city, type).without_geometry.all.map do |el|
       el.raw_feature(geometry: geoms[el.id])
     end
 
