8a02d97c13271ce9602dfcc62715dcd6e6ab5256
Bruno Salerno <br.salerno@gmail.com>
2019-03-24 23:41:00 +0100

    Restor oj dump in api

--- api/app/controllers/api.rb
+++ api/app/controllers/api.rb
@@ -122,7 +122,7 @@ class Api < App
 
     last_modified last_modified_source(@city, type)
 
-    formatted_lines_features_collection(@city, type).to_json
+    Oj.dump(formatted_lines_features_collection(@city, type).to_hash)
   end
 
   get '/:url_name/raw_source/:type' do |url_name, type|
@@ -130,7 +130,7 @@ class Api < App
 
     last_modified last_modified_source(@city, type)
 
-    lines_features_collection(@city, type).to_json
+    Oj.dump(lines_features_collection(@city, type).to_hash)
   end
 
   get '/editor/:url_name/data' do |url_name|
