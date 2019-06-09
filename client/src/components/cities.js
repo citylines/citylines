cfc8ee5356123c77da2b08b63d256ef4776f9aea
Bruno Salerno <br.salerno@gmail.com>
2019-06-09 13:04:16 +0200

    New social media layout

--- client/src/components/cities.js
+++ client/src/components/cities.js
@@ -210,10 +210,10 @@ class Cities extends Component {
         <div className="o-container o-container--medium" style={{textAlign: 'center', marginTop:30}}>
           <div className="u-letter-box--super">
             <p className="c-paragragh">
-              <span className="contact-icon"><a className="c-link" target="_blank" href="https://twitter.com/citylines_co"><span className="fab fa-twitter" /> Twitter</a></span>
-              <span className="contact-icon"><a className="c-link" target="_blank" href="mailto:info@citylines.co"><span className="fa fa-envelope"/> Email</a></span>
-              <span className="contact-icon"><a className="c-link" target="_blank" href="https://github.com/BrunoSalerno/citylines"><span className="fab fa-github" /> Github</a></span>
-              <span className="contact-icon"><a className="c-link" target="_blank" href="https://www.kaggle.com/citylines/city-lines"><span className="fab fa-kaggle" /> Kaggle</a></span>
+              <span className="contact-icon"><a target="_blank" href="https://twitter.com/citylines_co"><span className="fab fa-twitter" /></a></span>
+              <span className="contact-icon"><a target="_blank" href="mailto:info@citylines.co"><span className="fa fa-envelope"/></a></span>
+              <span className="contact-icon"><a target="_blank" href="https://github.com/BrunoSalerno/citylines"><span className="fab fa-github" /></a></span>
+              <span className="contact-icon"><a target="_blank" href="https://www.kaggle.com/citylines/city-lines"><span className="fab fa-kaggle" /></a></span>
             </p>
             <p><Translate content="cities.support" /> <a className="c-link" target="_blank" href="https://gitter.im/citylines/Lobby?utm_source=share-link&utm_medium=link&utm_campaign=share-link"><Translate content="cities.support_link" /></a></p>
           </div>
