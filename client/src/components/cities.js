d81c6d0cd28efcd0e12611dd773f73bd9d5587ed
Bruno Salerno <br.salerno@gmail.com>
2019-06-09 12:21:14 +0200

    Update cities brand icons

--- client/src/components/cities.js
+++ client/src/components/cities.js
@@ -210,10 +210,10 @@ class Cities extends Component {
         <div className="o-container o-container--medium" style={{textAlign: 'center', marginTop:30}}>
           <div className="u-letter-box--super">
             <p className="c-paragragh">
-              <span className="contact-icon"><a className="c-link" target="_blank" href="https://twitter.com/citylines_co"><span className="fa fa-twitter" /> Twitter</a></span>
-              <span className="contact-icon"><a className="c-link" target="_blank" href="mailto:info@citylines.co"><span className="fa fa-envelope"/> Email</a></span>
-              <span className="contact-icon"><a className="c-link" target="_blank" href="https://github.com/BrunoSalerno/citylines"><span className="fa fa-github" /> Github</a></span>
-              <span className="contact-icon"><a className="c-link" target="_blank" href="https://www.kaggle.com/citylines/city-lines"><b>K</b> Kaggle</a></span>
+              <span className="contact-icon"><a className="c-link" target="_blank" href="https://twitter.com/citylines_co"><span className="fab fa-twitter" /> Twitter</a></span>
+              <span className="contact-icon"><a className="c-link" target="_blank" href="mailto:info@citylines.co"><span className="fab fa-envelope"/> Email</a></span>
+              <span className="contact-icon"><a className="c-link" target="_blank" href="https://github.com/BrunoSalerno/citylines"><span className="fab fa-github" /> Github</a></span>
+              <span className="contact-icon"><a className="c-link" target="_blank" href="https://www.kaggle.com/citylines/city-lines"><span className="fab fa-kaggle" /> Kaggle</a></span>
             </p>
             <p><Translate content="cities.support" /> <a className="c-link" target="_blank" href="https://gitter.im/citylines/Lobby?utm_source=share-link&utm_medium=link&utm_campaign=share-link"><Translate content="cities.support_link" /></a></p>
           </div>
