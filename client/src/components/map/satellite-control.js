aad71ee5c6268c09e34b2460cc3f2c9ff0586ce9
Bruno Salerno <br.salerno@gmail.com>
2019-07-13 23:43:16 -0300

    Improve code

--- client/src/components/map/satellite-control.js
+++ client/src/components/map/satellite-control.js
@@ -10,22 +10,7 @@ class SatelliteControl {
 
     const button = document.createElement('button');
     button.className = 'mapbox-gl-draw_ctrl-draw-btn fa fa-globe-americas';
-    button.onclick = () => {
-      const currentStyle = this._map.getStyle();
-
-      const layers = currentStyle.layers.filter(l => (l.id.includes('sections_') || l.id.includes('stations_')));
-      const sources = {
-        sections_source: currentStyle.sources.sections_source,
-        stations_source: currentStyle.sources.stations_source
-      };
-
-      this._map.setStyle('mapbox://styles/mapbox/satellite-v9');
-
-      this._map.once('styledata', () => {
-        Object.entries(sources).map(([name, source]) => this._map.addSource(name, source));
-        layers.map(l => this._map.addLayer(l));
-      });
-    }
+    button.onclick = this.switchBaseMap.bind(this);
     this._container.appendChild(button);
 
     return this._container;
@@ -34,6 +19,31 @@ class SatelliteControl {
   onRemove() {
     this._container.parentNode.removeChild(this._container);
     this._map = undefined;
+  }
+
+  switchBaseMap() {
+    const currentData = this.fetchSourcesAndLayers();
+    this._map.setStyle(this._SATELLITE_STYLE);
+    this._map.once('styledata', () => {
+      this.restoreSourcesAndLayers(currentData.sources, currentData.layers);
+    });
+  }
+
+  fetchSourcesAndLayers() {
+    const currentStyle = this._map.getStyle();
+
+    const layers = currentStyle.layers.filter(l => (l.id.includes('sections_') || l.id.includes('stations_')));
+    const sources = {
+      sections_source: currentStyle.sources.sections_source,
+      stations_source: currentStyle.sources.stations_source
+    };
+
+    return {layers: layers, sources: sources};
+  }
+
+  restoreSourcesAndLayers(sources, layers) {
+    Object.entries(sources).map(([name, source]) => this._map.addSource(name, source));
+    layers.map(l => this._map.addLayer(l));
   }
 }
 
