bf5d480e00c4347060ee767c30e3860095923faf
Bruno Salerno <br.salerno@gmail.com>
2019-10-05 17:45:05 +0200

    Add ids

--- api/app/helpers/data_helpers.rb
+++ api/app/helpers/data_helpers.rb
@@ -4,10 +4,12 @@ module DataHelpers
       transport_mode = line.transport_mode
       system = line.system
       {
+        id: line.id,
         name: line.name,
         url_name: line.url_name,
         color: line.color,
-        system: system.name,
+        system_id: system.id,
+        system_name: system.name,
         transport_mode_id: transport_mode.id,
         transport_mode_name: transport_mode.name
       }
