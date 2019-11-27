3a5b0dbee9b2c27c53a4e4a87a6678c9d0a0fdb8
Bruno Salerno <br.salerno@gmail.com>
2019-11-27 18:48:18 +0100

    Fix size and position of inline avatar

--- client/src/components/user/avatar.js
+++ client/src/components/user/avatar.js
@@ -2,7 +2,7 @@ import React, {PureComponent} from 'react';
 
 class Avatar extends PureComponent {
   render() {
-    const size = `avatar-${this.props.inline ? 'inline u-small' : 'main'}`;
+    const size = `avatar-${this.props.inline ? 'inline u-xsmall' : 'main'}`;
     return (
       <div className={`c-avatar ${size}`}>
       <img className="c-avatar__img" alt="placeholder" src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50"></img>
