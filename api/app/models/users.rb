32628bedbccd3c0e1b1e056a9a3b5d7257f58df5
Bruno Salerno <br.salerno@gmail.com>
2019-11-27 21:02:04 +0100

    Initials may come from nickname

--- api/app/models/users.rb
+++ api/app/models/users.rb
@@ -1,17 +1,28 @@
 class User < Sequel::Model
   plugin :timestamps, :update_on_create => true
 
-  def nickname
+  def default_nickname
     self.name.split(" ").first
+  end
+
+  def nickname
+    default_nickname
   end
 
   def initials
     parts = self.name.split(" ")
+
+    # The default nickname is the first name
+    if parts.first != nickname then
+      parts = self.nickname.split(" ")
+    end
+
     res = unless parts[1].blank?
             parts[0][0] + parts[1][0]
           else
             parts[0][0..1]
           end
+
     res.upcase
   end
 end
