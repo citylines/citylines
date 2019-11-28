ab542f6971d5e0a3096250927fbbb3c1c7a156a0
Bruno Salerno <br.salerno@gmail.com>
2019-11-28 15:13:47 +0100

    Update nickname editor

--- client/src/components/user/nickname.js
+++ client/src/components/user/nickname.js
@@ -5,8 +5,7 @@ class Nickname extends Component {
     super(props, context);
     this.state = {
       edit: false,
-      name: this.props.name,
-      inputWidth: this.calcInputWidth(this.props.name)
+      name: this.props.name
     }
   }
 
@@ -14,39 +13,58 @@ class Nickname extends Component {
     const newName = e.target.value;
     this.setState({
       name: newName,
-      inputWidth: this.calcInputWidth(newName)
+      inputWidth: this.nameDiv.offsetWidth
     })
   }
 
-  toggleEdit() {
-    this.setState({edit: !this.state.edit}, () => {
-      if (this.state.edit) this.editInput.focus();
-    });
-  }
+  toggleEdit(e) {
+    const cancel = e.target.name == 'cancel';
+    let name = this.state.name;
 
-  calcInputWidth(text) {
-    let length = text.length * 0.5;
-    if (length < 4) length = 4;
-    return length + 'ch';
+    if (cancel) {
+      name = this.props.name
+    }
+
+    this.setState({
+      edit: !this.state.edit,
+      name: name,
+      inputWidth: this.nameDiv.offsetWidth
+    }, () => {
+      if (this.state.edit) {
+        this.editInput.focus();
+      } else {
+        if (!cancel) {
+        // TODO: save
+        }
+      }
+    });
   }
 
   render() {
     return (
-    <div style={{display:'inline'}}>
-      {this.state.edit ?
-      <input
-        ref={(input) => { this.editInput = input; }}
-        className="c-field user-page-username user-avatar-input"
-        value={this.state.name}
-        onChange={this.handleInputChange.bind(this)}
-        style={{width: this.state.inputWidth}}
-        ></input> :
-      <div className="user-page-username">
-        {this.props.name}
+      <div style={{display:'inline'}}>
+        <div className='user-page-username'>
+        {this.state.edit ?
+        <input
+          ref={(input) => { this.editInput = input; }}
+          className="c-field user-nickname-input"
+          value={this.state.name}
+          onChange={this.handleInputChange.bind(this)}
+          style={{width: this.state.inputWidth}}
+          ></input> : ''}
+        <div
+          className='user-current-nickname'
+          ref={(el) => { this.nameDiv = el; }}
+          style={{visibility: this.state.edit ? 'hidden' : 'visible'}}
+        >
+          {this.state.name}
+        </div>
       </div>
-      }
+
       {this.props.myProfile &&
       <a className="c-link user-avatar-edit" onClick={this.toggleEdit.bind(this)}>{this.state.edit ? 'Save' : 'Edit'}</a>}
+      {this.state.edit &&
+      <a className="c-link user-avatar-edit" name="cancel" onClick={this.toggleEdit.bind(this)}>Cancel</a>}
     </div>
     )
   }
