ac7a91598c83a6fd56bdf2661574f780a6c4bd64
Bruno Salerno <br.salerno@gmail.com>
2020-06-25 20:18:09 -0300

    Improve log legibility

--- api/tasks/stats.rb
+++ api/tasks/stats.rb
@@ -3,11 +3,11 @@ include CityHelpers
 namespace :stats do
   desc "Update city and system stats"
   task :update do
-    puts "> Stats updater"
+    puts "--> Stats updater"
 
     time_frame = ENV['TIME_FRAME'] ? ENV['TIME_FRAME'].to_i : 70
 
-    puts "Using time frame of #{time_frame} minutes"
+    puts "=> Using time frame of #{time_frame} minutes"
 
     time_limit = Time.now - time_frame * 60
     city_ids = Section.where{updated_at > time_limit}.select(:city_id).
@@ -16,12 +16,12 @@ namespace :stats do
       union(DeletedFeature.where(feature_class: 'Section').where{created_at > time_limit}.select(:city_id)).
       distinct(:city_id).all.map(&:city_id)
 
-    puts "#{city_ids.count} cities to update"
+    puts "=> #{city_ids.count} cities to update"
 
     city_ids.each do |city_id|
       city = City[city_id]
 
-      puts "Updating #{city.name} [#{city_id}]"
+      puts "=> Updating #{city.name} [#{city_id}]"
 
       city.systems.map do |system|
         system.length = system_length(system)
