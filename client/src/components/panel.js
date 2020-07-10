0498ce52d1b837341e23aba9741da02cee86390e
Bruno Salerno <br.salerno@gmail.com>
2020-07-10 17:39:18 -0300

    Add same logic to panel header title

--- client/src/components/panel.js
+++ client/src/components/panel.js
@@ -12,10 +12,12 @@ class PanelHeader extends PureComponent {
       <div className="panel-header o-grid__cell o-grid__cell--width-100">
         <div className="panel-header-title">
           <h3 className="c-heading">{this.props.name}</h3>
-          {!this.props.loading &&
-            <Link className="c-link" to={linkTo}>{linkLabel}</Link>}
-          {!this.props.loading && !editPath &&
-            <Translate className="c-link" component={Link} to={`/compare?cities=${this.props.urlName},`} content="compare.link" />}
+          <span className="commands">
+            {!this.props.loading &&
+              <Link className="c-link" to={linkTo}>{linkLabel}</Link>}
+            {!this.props.loading && !editPath &&
+              <Translate className="c-link" component={Link} to={`/compare?cities=${this.props.urlName},`} content="compare.link" />}
+          </span>
         </div>
       </div>
     );
