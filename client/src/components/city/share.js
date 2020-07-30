4ea09c6e79d68d0b925a2d7196d3029becf2b81e
Bruno Salerno <br.salerno@gmail.com>
2020-07-30 12:21:50 -0300

    Copy URL

--- client/src/components/city/share.js
+++ client/src/components/city/share.js
@@ -1,19 +1,35 @@
 import React, {PureComponent} from 'react';
 import CityToggleableContainer from './toggleable-container';
+import {CopyToClipboard} from 'react-copy-to-clipboard';
 
 class CityShare extends PureComponent {
+
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      url: window.location.href,
+      copied: false
+    };
+  }
+
+  handleInputChange(e) {
+    this.setState({url: e.target.value, copied: false});
+  }
+
   render() {
-    // TODO: use https://www.npmjs.com/package/react-copy-to-clipboard to copy
     // TODO: use https://github.com/nygardk/react-share for social buttons
     return (
       <CityToggleableContainer>
         <div className="c-input-group">
           <div className="o-field">
-            <input className="c-field" defaultValue={window.location.href} />
+            <input className={`c-field ${this.state.copied && 'c-field--success'}`} value={this.state.url} onChange={this.handleInputChange.bind(this)}/>
           </div>
-          <button className="c-button c-button--ghost">
-            <span className="fa fa-clipboard"></span>
-          </button>
+          <CopyToClipboard text={this.state.url}
+            onCopy={() => this.setState({copied: true})}>
+            <button className={`c-button ${this.state.copied ? 'c-button--success' : 'c-button--ghost'}`}>
+              <span className="fa fa-clipboard"></span>
+            </button>
+          </CopyToClipboard>
         </div>
       </CityToggleableContainer>
     )
