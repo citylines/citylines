8046f33f06d2a60fe0bdaa8aef281ceca030ecea
Bruno Salerno <br.salerno@gmail.com>
2020-08-24 02:03:10 -0300

    Remove useless parenthesis

--- api/app/helpers/popup_helpers.rb
+++ api/app/helpers/popup_helpers.rb
@@ -16,7 +16,7 @@ module PopupHelpers
   def hex_to_rgb_array(color)
     if color.length === 3
       color = color[0] + color[0] + color[1] + color[1] + color[2] + color[2]
-    elsif (color.length != 6)
+    elsif color.length != 6
       raise 'Invalid hex color: ' + color
     end
 
