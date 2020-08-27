356abec51fecbaa0879883de741c2858335f2b95
Bruno Salerno <br.salerno@gmail.com>
2020-08-27 19:11:21 -0300

    Add some i18n

--- client/src/components/city/feature-popup-content.js
+++ client/src/components/city/feature-popup-content.js
@@ -1,13 +1,13 @@
 import React, {Component} from 'react';
 import Translate from 'react-translate-component';
+import counterpart from 'counterpart';
 import {formatNumber} from '../../lib/number-tools';
 
 /**** Helpers ****/
 
 const validFeatureValue = (value) => value !== null && value !== 0 && value !== 999999;
 
-// FIXME: Replace 'Today' with i18n.
-const validOrToday = (value) => validFeatureValue(value) ? value : 'Today';
+const validOrToday = (value) => validFeatureValue(value) ? value : counterpart('city.popup.today');
 
 const lineStyle = (line) => ({
   color: line.label_font_color,
@@ -60,7 +60,7 @@ class FeaturePopupContent extends Component {
               <li className="c-list__item">
                 <Translate
                   className="station-popup"
-                  content={`city.popup.${this.fProps().name ? '' : 'unnamed_'}station`}
+                  content={`city.popup.${this.fProps().name ? 'named' : 'unnamed'}_station`}
                   with={{name: this.fProps().name}}
                 />
               </li>
@@ -107,6 +107,8 @@ class DetailedData extends Component {
       filter(line => currentUrlNames.indexOf(line.url_name) == -1).
       sort((a,b) => a.from && b.from && a.from > b.from ? 1 : -1);
 
+    const i18nFeaturekey = this.props.isStation ? 'station' : 'track';
+
     return (
       <div>
         <input className="popup-data-checkbox" id={this.props.id} type='checkbox'></input>
@@ -117,7 +119,7 @@ class DetailedData extends Component {
             ) }
           </li>
           <li className="c-list__item popup-data-title">
-            { !this.props.isStation && <Translate content="city.popup.track" /> }
+            <Translate content={`city.popup.${i18nFeaturekey}_details`} />
           </li>
           { this.props.length &&
             <li className="c-list__item"><Translate content="city.popup.length" with={{km: formatNumber(parseFloat(this.props.length)/1000)}} /></li>}
@@ -129,7 +131,7 @@ class DetailedData extends Component {
               <li className="c-list__item"><Translate content="city.popup.closure" with={{year: this.props.feature_closure}} /></li> }
           { otherLines.length > 0 &&
             <li className="c-list__item popup-data-title">
-              Other lines on the same {this.props.isStation ? 'station' : 'track'}
+              <Translate content={`city.popup.other_lines_${i18nFeaturekey}`} />
             </li>}
           { otherLines.length > 0 && otherLines.map(line =>
             <LineLabel
