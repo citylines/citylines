45b2b58d9ba22bd9805f83431a6bc2b25a7dc691
Bruno Salerno <br.salerno@gmail.com>
2020-09-10 18:39:01 -0300

    Set missing entry generator

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import {render} from 'react-dom';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
+import Counterpart from 'counterpart';
 
 import Main from './components/main';
 
 import assets from './lib/assets-provider';
 assets.loadPaths(window.assetsPaths);
+
+Counterpart.setMissingEntryGenerator((key) => '');
 
 render(
     <Router>
