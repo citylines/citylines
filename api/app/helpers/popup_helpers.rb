6e6a60fdd0a27d766dfc9cfc2d328d9d6a6ba086
Bruno Salerno <br.salerno@gmail.com>
2020-09-15 18:28:55 -0300

    Add new methods

--- api/app/helpers/popup_helpers.rb
+++ api/app/helpers/popup_helpers.rb
@@ -73,4 +73,75 @@ module PopupHelpers
 
     data_by_key
   end
+
+  def popup_features_data2(features)
+    ids_by_kind = {}
+    features.split(',').map do |f|
+      class_name, id = f.split('-')
+      ids_by_kind[class_name] ||= []
+      ids_by_kind[class_name] << id
+    end
+
+    data_by_key = {}
+
+    query = if ids = ids_by_kind['Station']
+      feature_popup_query('Station', ids)
+    end
+
+    if ids = ids_by_kind['Section']
+      sections_query = feature_popup_query('Section', ids)
+      if query
+        puts query
+        puts sections_query
+        query = query.union(sections_query)
+      else
+        query = sections_query
+      end
+    end
+
+    query.all.each do |feature|
+      klass = feature.delete(:klass)
+      feature[:lines].each do |line|
+        line['label_font_color'] = line_label_font_color(line['color'])
+        line['from'] ||= feature[:buildstart_end]
+        line['to'] ||= feature[:feature_closure]
+      end
+      data_by_key[[klass, feature[:id]].join('-')] = feature
+    end
+
+    data_by_key.reject{|k,v| v.blank?}
+  end
+
+  def feature_popup_query(klass, ids)
+    table_name = klass == 'Section' ? 'sections' : 'stations'
+    aux_table_name = "#{table_name[0..-2]}_lines"
+    aux_table_id = "#{table_name[0..-2]}_id"
+
+    query = %Q{
+      select
+        id,
+        buildstart,
+        coalesce(opening,closure,#{FeatureCollection::Section::FUTURE}) as buildstart_end,
+        coalesce(closure,#{FeatureCollection::Section::FUTURE}) as feature_closure,
+        #{klass == 'Section' ? 'length' : 'null::int as length'},
+        #{klass == 'Station' ? 'name' : 'null as name'},
+        '#{klass}' as klass,
+        lines
+      from #{table_name}
+      left join lateral (
+        select
+          #{aux_table_id},
+          jsonb_agg(jsonb_build_object('name', lines.name,'url_name',lines.url_name, 'system', coalesce(systems.name,''),'transport_mode_name',transport_modes.name,'color',color,'from',fromyear,'to',toyear)) as lines
+        from #{aux_table_name}
+          left join lines on lines.id = #{aux_table_name}.line_id
+          left join systems on systems.id = system_id
+          left join transport_modes on lines.transport_mode_id = transport_modes.id
+        where #{aux_table_id} = #{table_name}.id
+        group by #{aux_table_id}
+       ) as lines_data on #{aux_table_id} = #{table_name}.id
+      where ?
+    }
+
+    DB.fetch(query, Sequel.expr(id: ids))
+  end
 end
