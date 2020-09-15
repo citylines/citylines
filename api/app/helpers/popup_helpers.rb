28cb7db8b8b4aaaaeb299aba027276555d92940d
Bruno Salerno <br.salerno@gmail.com>
2020-09-15 18:35:32 -0300

    Remove old method

--- api/app/helpers/popup_helpers.rb
+++ api/app/helpers/popup_helpers.rb
@@ -35,55 +35,6 @@ module PopupHelpers
 
     data_by_key = {}
 
-    ['Station','Section'].map do |class_name|
-      ids = ids_by_kind[class_name]
-      klass = Kernel.const_get(class_name)
-      klass.where(id: ids).order(:id).all.map do |feature|
-        buildstart_end = feature.opening || feature.closure || FeatureCollection::Section::FUTURE
-        feature_closure = feature.closure || FeatureCollection::Section::FUTURE
-        lines_join_table_method = "#{class_name.downcase}_lines"
-
-        lines = feature.send(lines_join_table_method).map do |sl|
-          line = sl.line
-          {
-            name: line.name,
-            url_name: line.url_name,
-            system: line.system.name,
-            transport_mode_name: line.transport_mode.name,
-            color: line.color,
-            label_font_color: line_label_font_color(line.color),
-            from: sl.fromyear || buildstart_end,
-            to: sl.toyear || feature_closure
-          }
-        end
-
-        # We override buildstart and buildstart_end with
-        # feature data (not line data). And we add feature
-        # closure data.
-        data_by_key[[class_name, feature.id].join('-')] = {
-          buildstart: feature.buildstart,
-          buildstart_end: buildstart_end,
-          feature_closure: feature_closure,
-          length: feature.is_a?(Section) && feature.length,
-          name: feature.is_a?(Station) && feature.name,
-          lines: lines
-        }.reject{|k,v| v.blank?}
-      end
-    end
-
-    data_by_key
-  end
-
-  def popup_features_data2(features)
-    ids_by_kind = {}
-    features.split(',').map do |f|
-      class_name, id = f.split('-')
-      ids_by_kind[class_name] ||= []
-      ids_by_kind[class_name] << id
-    end
-
-    data_by_key = {}
-
     query = if ids = ids_by_kind['Station']
       feature_popup_query('Station', ids)
     end
