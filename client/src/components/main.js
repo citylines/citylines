adf8bacadcc887c0679e2b15e87e4bf5c2907c79
christaylor19 <christaylormail.94@gmail.com>
2020-10-15 00:48:54 +0100

    Fixing tab order of header

--- client/src/components/main.js
+++ client/src/components/main.js
@@ -1,6 +1,6 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { Switch, Route, Link } from 'react-router-dom';
-import MainStore from '../stores/main-store.js'
+import MainStore from '../stores/main-store.js';
 import CookieNotice from './cookie-notice.js';
 import BrowserCookies from 'browser-cookies';
 import Translate from 'react-translate-component';
@@ -25,14 +25,14 @@ class Main extends Component {
     this.bindedOnChange = this.onChange.bind(this);
 
     this.previousPathname = null;
-    this.unlisten = this.props.history.listen( loc =>  {
+    this.unlisten = this.props.history.listen((loc) => {
       if (loc.pathname != this.previousPathname) {
         // This fixes a bug in Safari:
         // the scroll is not set to zero when changing routes
         document.getElementById('main-container').scrollTop = 0;
 
         this.previousPathname = loc.pathname;
-        ga("send", "pageview", loc.pathname);
+        ga('send', 'pageview', loc.pathname);
       }
     });
   }
@@ -55,17 +55,19 @@ class Main extends Component {
   }
 
   displayMenu() {
-    return !this.props.location.pathname.match(/\/$|\user\/|\/auth|\/data|\/terms|\/compare/);
+    return !this.props.location.pathname.match(
+      /\/$|\user\/|\/auth|\/data|\/terms|\/compare/
+    );
   }
 
-  togglePanel() {
+  togglePanel() {
     MainStore.togglePanel();
   }
 
   async checkAuth() {
     const url = '/api/auth/check';
 
-    const response = await fetch(url, {credentials: 'same-origin'});
+    const response = await fetch(url, { credentials: 'same-origin' });
     const json = await response.json();
 
     if (json.userid) {
@@ -80,55 +82,96 @@ class Main extends Component {
   }
 
   onAcceptCookie() {
-    BrowserCookies.set('cookie-advice-accepted', "yes", {expires: 365});
+    BrowserCookies.set('cookie-advice-accepted', 'yes', { expires: 365 });
     MainStore.hideCookieAdvice();
   }
 
   render() {
     return (
-        <div>
-          <nav className="c-nav c-nav--inline">
-              <span className="c-nav__item" style={{display: this.displayMenu() ? 'inline-block' : 'none'}} onClick={this.togglePanel}>
-                  <span className="fa fa-bars"></span>
-              </span>
-              <Link to="/" className="c-nav__item c-text--loud">
-                <img src={assets.path("img/citylines-navbar.svg")} className="navbar-logo" />
-              </Link>
-              { this.state.userid ?
-              <Link to={`/user/${this.state.userid}`} className="c-nav__item c-nav__item--right"><Avatar size='inline' initials={this.state.initials} img={this.state.img}/></Link>  :
-              <Link to="/auth" className="c-nav__item c-nav__item--right"><Translate content="main.log_in" /></Link> }
-              <Link to="/terms" className="c-nav__item c-nav__item--right">
-                <Translate content="terms.title" />
-              </Link>
-              <Link to="/data" className="c-nav__item c-nav__item--right">
-                <Translate content="data.short_title" />
-              </Link>
-              <Link to="/compare" className="c-nav__item c-nav__item--right">
-                <Translate content="compare.short_title" />
-              </Link>
-          </nav>
-          <div id="main-container" className={`o-grid o-panel o-panel--nav-top ${this.state.loading ? 'loading' : null}`}>
-            <React.Suspense fallback={<SuspenseLoader />}>
-              <Switch>
-                <Route exact path="/" component={Home} />
-                <Route path="/auth" component={Auth} />
-                <Route path="/terms" component={Terms} />
-                <Route path="/data" component={Data} />
-                <Route path="/error" component={Error} />
-                <Route path="/compare" component={CityComparison} />
-                <Route path="/user/:user_id" component={User} />
-                <Route path="/:city_url_name" component={City} />
-              </Switch>
-            </React.Suspense>
-          </div>
-          <div className="u-center-block__content" style={{display: this.state.loading ? 'block' : 'none', width:'200px'}}>
-            <div className="loader"></div>
-          </div>
-          {this.state.showCookieAdvice && <CookieNotice
-            onAccept={this.onAcceptCookie}
-            />}
+      <div>
+        <nav
+          className="c-nav c-nav--inline"
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            alignItems: 'center',
+          }}
+        >
+          <span
+            className="c-nav__item"
+            style={{ display: this.displayMenu() ? 'inline-block' : 'none' }}
+            onClick={this.togglePanel}
+          >
+            <span className="fa fa-bars"></span>
+          </span>
+          <Link to="/" tabindex={0} className="c-nav__item c-text--loud">
+            <img
+              src={assets.path('img/citylines-navbar.svg')}
+              className="navbar-logo"
+            />
+          </Link>
+          <div style={{ flexGrow: 2 }} />
+          <Link tabindex={0} to="/compare" className="c-nav__item">
+            <Translate content="compare.short_title" />
+          </Link>
+          <Link tabindex={0} to="/data" className="c-nav__item">
+            <Translate content="data.short_title" />
+          </Link>
+          <Link tabindex={0} to="/terms" className="c-nav__item">
+            <Translate content="terms.title" />
+          </Link>
+
+          {this.state.userid ? (
+            <Link
+              tabindex={0}
+              to={`/user/${this.state.userid}`}
+              className="c-nav__item"
+            >
+              <Avatar
+                size="inline"
+                initials={this.state.initials}
+                img={this.state.img}
+              />
+            </Link>
+          ) : (
+            <Link tabIndex={0} to="/auth" className="c-nav__item">
+              <Translate content="main.log_in" />
+            </Link>
+          )}
+        </nav>
+        <div
+          id="main-container"
+          className={`o-grid o-panel o-panel--nav-top ${
+            this.state.loading ? 'loading' : null
+          }`}
+        >
+          <React.Suspense fallback={<SuspenseLoader />}>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/auth" component={Auth} />
+              <Route path="/terms" component={Terms} />
+              <Route path="/data" component={Data} />
+              <Route path="/error" component={Error} />
+              <Route path="/compare" component={CityComparison} />
+              <Route path="/user/:user_id" component={User} />
+              <Route path="/:city_url_name" component={City} />
+            </Switch>
+          </React.Suspense>
         </div>
-      )
+        <div
+          className="u-center-block__content"
+          style={{
+            display: this.state.loading ? 'block' : 'none',
+            width: '200px',
+          }}
+        >
+          <div className="loader"></div>
+        </div>
+        {this.state.showCookieAdvice && (
+          <CookieNotice onAccept={this.onAcceptCookie} />
+        )}
+      </div>
+    );
   }
 }
 
@@ -141,9 +184,9 @@ class SuspenseLoader extends Component {
     MainStore.unsetLoading();
   }
 
-  render(){
+  render() {
     return null;
   }
 }
 
-export default Main
+export default Main;
