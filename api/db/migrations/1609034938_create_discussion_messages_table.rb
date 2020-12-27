659e354e645439c74bc15bd5e5097e82fa0fb1d0
Bruno Salerno <br.salerno@gmail.com>
2020-12-27 19:18:02 -0300

    Add index

--- api/db/migrations/1609034938_create_discussion_messages_table.rb
+++ api/db/migrations/1609034938_create_discussion_messages_table.rb
@@ -2,7 +2,7 @@ Sequel.migration do
   change do
     create_table(:discussion_messages) do
       primary_key :id
-      foreign_key :city_id, :cities
+      foreign_key :city_id, :cities, index: true
       foreign_key :user_id, :users
       String      :content, text: true, null: false
       DateTime    :created_at
