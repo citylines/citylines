41895bc79a4e879146538ecbc4056dc7e6baf704
Bruno Salerno <br.salerno@gmail.com>
2020-12-28 11:55:12 -0300

    Use contrasting color

--- client/src/lib/map-to-img.js
+++ client/src/lib/map-to-img.js
@@ -9,8 +9,9 @@ const downloadImgFromMapCanvas = (urlName, mapCanvas, format = "image/png", qual
   // Add watermark to the copy
   const watermark = `citylines.co/${urlName}`;
   copyCtx.font = "35px Arial";
-  const width = copyCtx.measureText(watermark).width;
-  copyCtx.fillText(watermark, copy.width - width - 90, copy.height - 70);
+  const coords = getWatermarkCoords(copyCtx, watermark);
+  copyCtx.fillStyle = getContrastingColor(copyCtx, coords[0], coords[1]);
+  copyCtx.fillText(watermark, coords[0], coords[1]);
 
   copy.toBlob(blob => {
     const anchor = document.createElement('a');
@@ -22,6 +23,19 @@ const downloadImgFromMapCanvas = (urlName, mapCanvas, format = "image/png", qual
   format,
   quality,
   );
+}
+
+const getWatermarkCoords = (context, watermark) => {
+  const width = context.measureText(watermark).width;
+  const x = context.canvas.width - width - 90;
+  const y = context.canvas.height - 70;
+  return [x, y];
+}
+
+const getContrastingColor = (context, x, y) => {
+  const rgb = context.getImageData(x, y, 1, 1).data;
+  const luma = (0.2126 * rgb[0]) + (0.7152 * rgb[1]) + (0.0722 * rgb[2]);
+  return luma >= 165 ? "#000" : "#fff";
 }
 
 export default downloadImgFromMapCanvas
