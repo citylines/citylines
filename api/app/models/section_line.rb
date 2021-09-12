94ec0a7342edec372843d49cfbe107c389d6e506
Bruno Salerno <br.salerno@gmail.com>
2021-09-12 13:42:38 -0300

    Destroy line groups when destroying section_lines or station_lines

--- api/app/models/section_line.rb
+++ api/app/models/section_line.rb
@@ -2,4 +2,10 @@ class SectionLine < Sequel::Model
   plugin :timestamps, :update_on_create => true
 
   many_to_one :line
+  one_to_many :section_line_groups
+
+  def before_destroy
+    self.section_line_groups.map(&:destroy)
+    super
+  end
 end
--- api/app/models/station_line.rb
+++ api/app/models/station_line.rb
@@ -2,4 +2,10 @@ class StationLine < Sequel::Model
   plugin :timestamps, :update_on_create => true
 
   many_to_one :line
+  one_to_many :station_line_groups
+
+  def before_destroy
+    self.station_line_groups.map(&:destroy)
+    super
+  end
 end
