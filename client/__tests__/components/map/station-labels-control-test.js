d8ea8391ad16d23ca8ae5632aa36ca9a4fb77764
Bruno Salerno <br.salerno@gmail.com>
2022-05-01 11:45:10 -0300

    Add tests for new control

---
+++ client/__tests__/components/map/station-labels-control-test.js
@@ -0,0 +1,23 @@
+import StationLabelsControl from "../../../src/components/map/station-labels-control";
+
+describe("StationLabelsControl", () => {
+  it("should set and return the right state", () => {
+    const container = document.createElement('div');
+    const mockOnClick = jest.fn();
+
+    const control = new StationLabelsControl({
+      container: container,
+      showStationLabels: false,
+      onClick: () => {mockOnClick()},
+    });
+    control.onAdd();
+
+    expect(control._button.className).toEqual("mapbox-gl-draw_ctrl-draw-btn fas fa-comment");
+
+    control.setState(true)
+    expect(control._button.className).toEqual("mapbox-gl-draw_ctrl-draw-btn fas fa-comment-slash");
+
+    control._button.onclick();
+    expect(mockOnClick.mock.calls.length).toBe(1);
+  });
+});
